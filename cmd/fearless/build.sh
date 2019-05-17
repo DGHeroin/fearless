1c091a51e04a14fbad9ca06a87331b9796f3da4b
ade <ade.li.indie@gmail.com>
2019-05-17 18:28:05 +0800

    build bin script

---
+++ cmd/fearless/build.sh
@@ -0,0 +1,7 @@
+build_path="./out"
+mkdir -p ${build_path}
+GOOS=linux   GOARCH=amd64 go build -o ${build_path}/fearless-linux
+GOOS=linux   GOARCH=386   go build -o ${build_path}/fearless-linux32
+GOOS=linux   GOARCH=arm GOARM=5 go build -o ${build_path}/fearless-arm5-rpi
+GOOS=windows GOARCH=amd64 go build -o ${build_path}/fearless-win.exe
+GOOS=windows GOARCH=386   go build -o ${build_path}/fearless-win32.exe
